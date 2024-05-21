6dd205bac3646dccbb33eae844e95ba4bd9e3db9
TJ Maxwell <tjmaxwell157@gmail.com>
2024-05-21 17:48:29 -0600

    add madder package initializer, register controller for unity editor

---
+++ Editor/MadderPackageInitializer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+#if UNITY_EDITOR
+[UnityEditor.InitializeOnLoad]
+#endif
+
+public static class MadderPackageInitializer
+{
+    static MadderPackageInitializer()
+    {
+        // Register the MadderController layout with the InputSystem
+        InputSystem.RegisterLayout<MadderController>();
+    }
+}
